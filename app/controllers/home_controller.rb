50db083612fecbf09d5f124b18656a29c8dd6793
techmoves <muhndlovu28@gmail.com>
2023-09-30 20:12:12 +0200

    Fix the linters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-      @categories = Category.all
+    @categories = Category.all
   end
 end
--- test/controllers/transfers_controller_test.rb
+++ test/controllers/transfers_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class TransfersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get transfers_index_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get transfers_new_url
     assert_response :success
   end
